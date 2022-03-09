8b11fcad99159a150a18d7169d0767bc459e1184
MohammedElseddik <rebbouh.mohammed.elseddik@gmail.com>
2022-03-09 15:03:44 +0100

    Structure the project files Add the mobile menu

--- main.js
+++ main.js
@@ -1,20 +1,26 @@
-const navBar = document.querySelector('.nav');
-const navList = document.querySelector('.nav__list');
-const toggoler = document.querySelector('.nav__toggler');
+const navBar = document.querySelector(".nav");
+const navList = document.querySelector(".nav__list");
+const navToggle = document.querySelector(".nav__toggler");
+const toggleFirstdBar = document.querySelector(".first-bar");
+const toggleSecondBar = document.querySelector(".second-bar");
+const toggleThirdBar = document.querySelector(".third-bar");
+const toggleBars = document.querySelector("toggler-bar");
+const navItems = document.querySelectorAll(".nav__item");
 
-
-function opneMenu () {
-  navBar.classList.toggle('collapsible');
-  navBar.classList.toggle("collapsible--expanded");
+function opneMenu() {
+  navItems.forEach((elem) => {
+    elem.classList.toggle("mobile-menu__item");
+    elem.classList.remove("active-item");
+  });
+  toggleSecondBar.classList.toggle("hidden");
+  toggleFirstdBar.classList.toggle("transform");
+  toggleThirdBar.classList.toggle("transform");
+  document.body.classList.toggle("no-scroll");
+  navBar.classList.toggle("nav");
+  navBar.classList.toggle("mobile-menu__nav");
+  navList.classList.toggle("mobile-menu");
 }
 
-
-toggoler.addEventListener('click', opneMenu)
-
-
-// const collapsibles = document.querySelectorAll(".collapsible");
-// collapsibles.forEach((item) =>
-//   item.addEventListener("click", function () {
-//     this.classList.toggle("collapsible--expanded");
-//   })
-// );
+['click', 'keypress'].forEach((event) => {
+  navToggle.addEventListener(event, opneMenu);
+});
